e33e8403f219a2011aa3a28f6662d54aeed7544c
Kev Zettler <kevzettler@gmail.com>
2011-01-03 20:38:42 -0800

    updated mongo initilizare to handle heroku environment

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,4 +1,14 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+if ENV['MONGOHQ_URL'] 
+    #MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+    MongoMapper.connection = Mongo::Connection.new(ENV["MONGOHQ_URL"]) 
+  else 
+    #MongoMapper.config = {RAILS_ENV => {'uri' => 'mongodb://localhost/sushi'}}
+    MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  end
+
+
+#MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+
 MongoMapper.database = "#bookmarks-#{Rails.env}"
 
 if defined?(PhusionPassenger)
