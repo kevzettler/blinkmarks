057d73cf69fd7109767c04227dc823b6eb1a84c1
Kev Zettler <kevzettler@gmail.com>
2011-02-19 18:21:16 -0800

    added initializer for externaljs cache

---
+++ config/initializers/external_js.rb
@@ -0,0 +1,29 @@
+class ExternalJS
+  @@cache = ""
+
+  def self.render
+    b = Proc.new do
+      if Rails.env == "staging" or Rails.env == "development"
+        easyXDM = 'easyXDM.debug.js'
+        root_url = "http://localhost:3000/"
+      else
+        easyXDM = 'easyXDM.min.js'
+        root_url = "http://localhost:3000/"
+      end
+      
+      binding
+    end.call
+
+    return ERB.new(File.read("#{RAILS_ROOT}/app/views/bar/external.html.erb")).result(b)
+  end
+
+  def self.update!
+    @@cache = ExternalJS.render
+  end
+  
+  def self.cache
+    @@cache
+  end
+end
+
+ExternalJS.update!
